8f5a0b69bff2e47494cad73bc842788e59db1c86
yasfachrezyy <apongaf21@gmail.com>
2024-11-09 13:56:55 +0700

    membuat form screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
 import 'package:aslab_travel/latihanmodul/pertemuan4.dart';
+import 'package:aslab_travel/screens/landingpage.dart';
 import 'package:aslab_travel/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +15,7 @@ class TravelApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: LandingPageOne(),
     );
   }
 }
